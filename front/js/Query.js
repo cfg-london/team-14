8a75632336964091de4a93fb95135b086b507977
Joseph <josephsgbku@gmail.com>
2017-11-17 22:29:59 +0000

    search bar functionality

--- front/js/Query.js
+++ front/js/Query.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 	$("#Query").click(function(){
 		string = $("#queryStr").val()
+         alert(string)
 		$.ajax({
             type: "GET",
             success: function(data) {
-            alert(string)
+           
             for(i = 0; i < data.length; i++){
                 $('ul').append('<li>'+data[i]+'</li>');
             }
