67459c80977a3a0a95229cc6e7a44f4aa01ff825
Joseph <josephsgbku@gmail.com>
2017-11-17 22:26:13 +0000

    jscript

--- front/js/Query.js
+++ front/js/Query.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
 	$("#Query").click(function(){
+		string = $("#queryStr").val()
 		$.ajax({
             type: "GET",
-            url: "/ajax/more/",
             success: function(data) {
+            alert(string)
             for(i = 0; i < data.length; i++){
                 $('ul').append('<li>'+data[i]+'</li>');
             }
