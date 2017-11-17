af201b7255a974ee6ba7335356af144af29d7eb3
Joseph <josephsgbku@gmail.com>
2017-11-17 22:09:43 +0000

    stuff

--- front/js/Query.js
+++ front/js/Query.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
 	$("#Query").click(function(){
 		$.ajax({
-			type: "GET",
+            type: "GET",
+            url: "/ajax/more/",
+            success: function(data) {
+            for(i = 0; i < data.length; i++){
+                $('ul').append('<li>'+data[i]+'</li>');
+            }
+        }
+        });
+	
+	});
 
-
-		})	
-
-		
-	})
-
-
-})
+});
