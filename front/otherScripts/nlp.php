f675721ca6a7838ccc30127c6be49e45e84e4daf
Force <force@ip-172-31-10-195.eu-west-1.compute.internal>
2017-11-18 08:18:41 +0000

    Script files

--- front/otherScripts/nlp.php
+++ front/otherScripts/nlp.php
@@ -1,7 +1,11 @@
 <?php 
 
-$command = escapeshellcmd('python processQuery.py "Who is albert einsein"');
+$command = escapeshellcmd('processQuery.py');
 $output = shell_exec($command);
+echo "NUJINIUNUI";
+$mystring = system('sudo /usr/bin/python /var/www/html/scripts/processQuery.py "Who is albert einstein"', $retval);
 echo $output;
+echo $mystring;
+echo $retval;
 
 ?>
