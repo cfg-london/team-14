2467eab183cce6f78b8241dd8d77e6ffad7ee4b7
Ruben9922 <Ruben9922@users.noreply.github.com>
2017-11-18 03:21:50 +0000

    Remove unwanted leading text

--- biography/biography.py
+++ biography/biography.py
@@ -1,5 +1,10 @@
 import urllib.request
 import ssl
+import re
 
-x = urllib.request.urlopen('https://www.nobelprize.org/nobel_prizes/peace/laureates/1964/king-facts.html', context=ssl.SSLContext())
-print(x.read())
+response = urllib.request.urlopen('https://www.nobelprize.org/nobel_prizes/peace/laureates/1964/king-facts.html', context=ssl.SSLContext())
+
+content = response.read().decode('utf-8')
+pattern1 = r"<p>\s*<strong>\s*Prize share:\s*</strong>\s*.*\s*</p>\s*</div>\s*</div>\s*</div>\s*<div style=\"clear:both;\"></div>\s*"
+content1 = re.split(pattern1, content)[1]
+print(content)
