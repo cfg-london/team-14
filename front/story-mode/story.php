152303be2131b662239c6289d433f1377ef07aa7
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2017-11-18 11:00:12 +0000

    Updated story mode php.

--- front/story-mode/story.php
+++ front/story-mode/story.php
@@ -4,7 +4,7 @@
         <?php
         // /story.php?laureate=id
             if (isset($_GET['laureate'])) {
-                echo $_GET['laureate'];
+                $id = $_GET['laureate'];
             }else{
                 // Fallback behaviour
             }
@@ -19,7 +19,37 @@
                 //Print out the contents.
                 $fullName = $data['laureates'][0]['firstname'] . " " . $data['laureates'][0]['surname'];
                 $surname = $data['laureates'][0]['surname'];
+                $year = $data['laureates'][0]['prizes'][0]['year'];
             }
+            $page_1 = array(
+                "6" => "She proved that atoms were divisible, which led to the creation of atomic physics.",
+                "167" => "His greatest achievement is considered the discovery of the atomic nucleus.",
+                "944" => "Switzerland's latest Nobel laureate said he had to overcome a fear of the dark, and a poor record in school, before reaching the apex of science.",
+                "217" => "He made significant scientific contributions to several different fields and is considered the father of molecular biology",
+                "292" => "He is known by many as the father of femtochemistry."
+            );
+
+            $page_2 = array(
+             "6" => "She received the prize in recognition of her services to the advancement of chemistry by the discovery of the elements radium and polonium, by the isolation of radium and the study of the nature and compounds of this remarkable element",
+             "167" => "He received the prize for his investigations into the disintegration of the elements, and the chemistry of radioactive substances.",
+             "944" => "He received the prize for developing cryo-electron microscopy for the high-resolution structure determination of biomolecules in solution",
+             "217" => "He received the prize for his research into the nature of the chemical bond and its application to the elucidation of the structure of complex substances",
+             "292" => "He received the prize for his studies of the transition states of chemical reactions using femtosecond spectroscopy"
+            );
+
+            $page_3 = array(
+                "6" => "In December 1903, Marie Curie became the first women to be awarded the Nobel Prize, when along with her husband and Henri Becquerel, she was awarded the Nobel Prize in Physics in recognition of the extraordinary services they have rendered by their joint researches on the radiation phenomena discovered by Professor Henri Becquerel. Initially the Committee didn’t want to recognize the contribution of Marie Curie but her husband was informed of this by a committee member, who was also an advocate of women scientists. Upon a complaint by Pierre, his wife’s name was added to the nomination. The award money allowed the Curies to hire their first lab assistant.",
+                "167" => "He invented a receiver to detect radio waves. Rutherford’s radio receiver became a part of the communications revolution known as wireless telegraphy.",
+                "944" => "Since I was a child I wanted to become a scientist, Dubochet told Swiss public broadcaster RTS in an interview. The Swiss Nobel laureate, who shared the Nobel Prize in Chemistry 2017 with two others, said however that he struggled in school.
+In fact: I was very bad in school, Dubochet told RTS. The scientist adds that a teacher diagnosed him as dyslexic when he was about to fail in high school. ",
+                "217" => "After World War II he and his wife, Ann became peace activists and in 1955 he joined Albert Einstein, Bertrand Russell and eight other scientists in signing the Russell-Einstein Manifesto warning against the dangers of nuclear weapons.",
+                "292" => "Chemical reactions in which molecules held together by atoms meet and reorganize into new compounds are one of nature's most fundamental processes. This transition from one constellation to another happens very quickly. The process is possible because the atoms inside a molecule vibrate. The time between these vibrations is very short - 10-100 femtoseconds. In the late 1980s Ahmed Zewail developed methods for studying chemical reactions in detail. By using laser technology to produce flashes of light just a few femtoseconds long, reactions can be mapped."
+            );
+            // Marie Curie - ID: 6
+            // Ernest Rutherford - ID: 167
+            // Jacques Dubochet - ID: 944
+            // Linus Pauling - ID: 217
+            // Ahmed Zewail - ID:  292
         ?>
         <title>Nobel Laureate Story</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -34,13 +64,15 @@
                 <div class="slide">
                         <div class="laureate-info">
                             <?php
-                            echo '<img class="rounded-circle" src="https://www.nobelprize.org/nobel_prizes/peace/laureates/1964/'. $surname .'_postcard.jpg" width="140" height="140">'; 
+                            echo '<img class="rounded-circle" src="https://www.nobelprize.org/nobel_prizes/peace/laureates/'.$year.'/'. $surname .'_postcard.jpg" width="140" height="140">';
                             echo '<h3>'.$fullName.'</h3>';
                             ?>
                         </div>
 
                         <div class="motivation">
-                            A very cool thing done by this laureate.
+                            <?php
+                                echo $page_1[$id];
+                             ?>
                         </div>
                 </div>
 
@@ -49,13 +81,16 @@
                         <div class="row featurette">
                             <div class="col-md-7 order-md-2">
                                 <h2 class="featurette-heading">
-                                    Something about the laureate.
-                                    <span class="text-muted">Something else.</span>
+                                    <?php
+                                        echo $fullName;
+                                        echo '<span class="text-muted">'. $year . ' </span>';
+                                        echo $page_2[$id];
+                                     ?>
                                 </h2>
                             </div>
 
                             <div class="col-md-5 order-md-1">
-                                <?php echo '<img class="rounded-circle" src="https://www.nobelprize.org/nobel_prizes/peace/laureates/1964/'. $surname .'_postcard.jpg" width="500" height="500">'; ?>
+                                <?php echo '<img class="rounded-circle" src="https://www.nobelprize.org/nobel_prizes/peace/laureates/'.$year.'/'. $surname .'_postcard.jpg" width="500" height="500">'; ?>
                             </div>
                         </div>
                     </div>
@@ -63,16 +98,18 @@
 
             	<div class="slide">
                     <div class="laureate-info">
-                        Yet more information maybe links
+                        <?php
+                            echo $page_3[$id];
+                         ?>
                     </div>
 
                     <div class="related-laureates">
                         <h4><span class="text-muted">Related laureates:</span></h4>
                         <ul>
-                            <li><img class="rounded-circle" src="https://www.nobelprize.org/nobel_prizes/peace/laureates/1964/king_postcard.jpg" width="100" height="100"></li>
-                            <li><img class="rounded-circle" src="https://www.nobelprize.org/nobel_prizes/peace/laureates/1964/king_postcard.jpg" width="100" height="100"></li>
-                            <li><img class="rounded-circle" src="https://www.nobelprize.org/nobel_prizes/peace/laureates/1964/king_postcard.jpg" width="100" height="100"></li>
-                            <li><img class="rounded-circle" src="https://www.nobelprize.org/nobel_prizes/peace/laureates/1964/king_postcard.jpg" width="100" height="100"></li>
+                            <li><img class="rounded-circle" src="https://www.nobelprize.org/nobel_prizes/peace/laureates/2009/ramakrishnan_postcard.jpg" width="100" height="100"></li>
+                            <li><img class="rounded-circle" src="https://www.nobelprize.org/nobel_prizes/peace/laureates/1935/joliot-curie_postcard.jpg" width="100" height="100"></li>
+                            <li><img class="rounded-circle" src="https://www.nobelprize.org/nobel_prizes/peace/laureates/2015/sancar_postcard.jpg" width="100" height="100"></li>
+                            <li><img class="rounded-circle" src="https://www.nobelprize.org/nobel_prizes/peace/laureates/2017/frank_postcard.jpg" width="100" height="100"></li>
                         </ul>
                     </div>
                 </div>
@@ -95,7 +132,7 @@
                     scrollBar: false,
                     autoScrolling: true,
                     scrollOverflow: true,
-                    sectionsColor: ['#d5ff80', '#80dfff', '#ffff66', '#ffb3b3', '#ff99ff'],
+                    sectionsColor: ['#333333', '#333333', '#333333', '#333333', '#333333'],
 
                 });
             });
