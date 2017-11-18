fb242ec38692969d7541ca848143c008fa724444
Ruben9922 <Ruben9922@users.noreply.github.com>
2017-11-18 03:27:04 +0000

    Remove unwanted trailing text

--- biography/biography.py
+++ biography/biography.py
@@ -7,5 +7,7 @@ response = urllib.request.urlopen('https://www.nobelprize.org/nobel_prizes/peace
 
 content = response.read().decode('utf-8')
 beginPattern = r"<p>\s*<strong>\s*Prize share:\s*</strong>\s*.*\s*</p>\s*</div>\s*</div>\s*</div>\s*<div style=\"clear:both;\"></div>\s*"
+endPattern = r"<div class=\"copy\">Copyright &copy;"
 content1 = re.split(beginPattern, content)[1]
-print(content)
+content2 = re.split(endPattern, content1)[0]
+print(content2)
