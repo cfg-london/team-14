04906445bcd54469c336c03ff7860b65091c9b1d
Ruben9922 <Ruben9922@users.noreply.github.com>
2017-11-18 03:23:31 +0000

    Rename variable

--- biography/biography.py
+++ biography/biography.py
@@ -6,6 +6,6 @@ import re
 response = urllib.request.urlopen('https://www.nobelprize.org/nobel_prizes/peace/laureates/1964/king-facts.html', context=ssl.SSLContext())
 
 content = response.read().decode('utf-8')
-pattern1 = r"<p>\s*<strong>\s*Prize share:\s*</strong>\s*.*\s*</p>\s*</div>\s*</div>\s*</div>\s*<div style=\"clear:both;\"></div>\s*"
-content1 = re.split(pattern1, content)[1]
+beginPattern = r"<p>\s*<strong>\s*Prize share:\s*</strong>\s*.*\s*</p>\s*</div>\s*</div>\s*</div>\s*<div style=\"clear:both;\"></div>\s*"
+content1 = re.split(beginPattern, content)[1]
 print(content)
