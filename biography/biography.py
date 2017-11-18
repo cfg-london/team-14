86823450d62b716539a304f8569756b8a38f790c
Ruben9922 <Ruben9922@users.noreply.github.com>
2017-11-18 03:55:38 +0000

    Minor formatting changes

--- biography/biography.py
+++ biography/biography.py
@@ -4,10 +4,11 @@ import re
 
 # Not checking server - must change this for production!
 response = urllib.request.urlopen('https://www.nobelprize.org/nobel_prizes/peace/laureates/1964/king-facts.html', context=ssl.SSLContext())
-
 content = response.read().decode('utf-8')
+
 beginPattern = r"<p>\s*<strong>\s*Prize share:\s*</strong>\s*.*\s*</p>\s*</div>\s*</div>\s*</div>\s*<div style=\"clear:both;\"></div>\s*"
-endPattern = r"<div class=\"copy\">Copyright &copy;"
 content1 = re.split(beginPattern, content)[1]
+endPattern = r"<div class=\"copy\">Copyright &copy;"
 content2 = re.split(endPattern, content1)[0]
+
 print(content2)
