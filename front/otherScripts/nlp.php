7cab2f19796e322455367b3f62c508fb36ce8229
Force <force@ip-172-31-10-195.eu-west-1.compute.internal>
2017-11-18 08:22:48 +0000

    Updated script

--- front/otherScripts/nlp.php
+++ front/otherScripts/nlp.php
@@ -1,11 +1,7 @@
 <?php 
 
-$command = escapeshellcmd('processQuery.py');
-$output = shell_exec($command);
-echo "NUJINIUNUI";
-$mystring = system('sudo /usr/bin/python /var/www/html/scripts/processQuery.py "Who is albert einstein"', $retval);
-echo $output;
-echo $mystring;
-echo $retval;
-
+$command = escapeshellcmd('/usr/bin/python /var/www/html/scripts/processQuery.py "Who is AQlbert inteS"');
+$output =  exec($command,$array);
+$mystring = system('/usr/bin/python /var/www/html/scripts/processQuery.py "Who is AQlbert inteS"',$retval1);
+echo $output
 ?>
--- front/otherScripts/processQuery.py
+++ front/otherScripts/processQuery.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import nltk
 import sys
 #nltk.download('punkt')
